4658698db7a8809501ef46c6e6708dafa5c863dd
jeffguam23 <jeffguam23@hotmail.it>
2019-05-16 20:51:08 +0300

    lion

---
+++ js/filter-content.js
@@ -0,0 +1,17 @@
+$('.filters ul li').click(function() {
+            $('.filters ul li').removeClass('active');
+            $(this).addClass('active');
+
+            var data = $(this).attr('data-filter');
+            $grid.isotope({
+                filter: data
+            })
+        });
+
+        var $grid = $(".img-content").isotope({
+            itemSelector: ".all",
+            percentPosition: true,
+            masonry: {
+                columnWidth: ".all"
+            }
+        })
